2a9f06d0b11f521b27874c53ddb5db96bf75b8f8
John Albert T. Presentacion <johnalbertpresentacion@gmail.com>
2025-05-30 22:11:42 +0800

    init

---
+++ src/main/java/com/SmartPark/SmartPark_API/SmartParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.SmartPark.SmartPark_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartPark/SmartPark_API/SmartParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartPark.SmartPark_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
